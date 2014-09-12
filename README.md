b7e13f78aacba1f8eb72a92176ad7c9340760067
Isaac Sadaqah <isaac@datadoghq.com>
2014-09-12 11:50:08 -0400

    Various changes (see details)
    1. Update README.md
    2. Add process count metrics
    3. Add server tags, and rename process tag
    4. Add service check message with all process details
    5. Update some status conversions

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-A simple supervisord process and uptime check.
+A simple supervisord process and uptime check. Check collects the following:
+
+1. Service check on each process (Critical, Warning, OK)
+2. Uptime of each process
+3. Process count by status metrics
 
 Getting Started
-------------------
+---------------
 1. Install or upgrade to Dataodg agent >= 5.0.
 2. Configure `inet_http_server` in `/etc/supervisord.conf` (username and password are optional):
 
@@ -11,14 +15,23 @@ Getting Started
     username:user
     password:pass
     ```
-
 4. Reload supervisor.
 5. Clone this repository.
-6. Edit supervisord.yaml to have your server details and the processes you want to monitor.
-7. Place supervisord.py and supervisord.yaml in the checks and configuration folders. Path to these folders can be viewed via the agent info command.
-8. Happy supervising!
+6. Install `xmlrpclib`.
+7. Edit supervisord.yaml to have your server details and the processes you want to monitor.
+8. Place supervisord.py and supervisord.yaml in the checks and configuration folders. Path to these folders can be viewed via the agent info command.
+9. Restart Datadog agent.
+10. Create alerts, query value widgets, event streams and graphs to monitor your processes.
+
+Happy supervisoring!
+
+Links and Resources
+-------------------
+1. [Datadog Agent on Github](https://github.com/DataDog/dd-agent/)
+2. [Datadog Documentation](http://docs.datadoghq.com/)
+3. [Supervisord XML-RPC API Documentation](http://supervisord.org/api.html)
 
 License
-----------
+-------
 MIT License
 
--- supervisord.py
+++ supervisord.py
@@ -1,33 +1,71 @@
+import time
+
 from checks import AgentCheck
 
 import xmlrpclib
 
 DEFAULT_HOST = 'localhost'
 DEFAULT_PORT = '9001'
+DEFAULT_SERVER = 'server'
+
+OK = AgentCheck.OK
+CRITICAL = AgentCheck.CRITICAL
+UNKNOWN = AgentCheck.UNKNOWN
+
 STATUS_MAP = {
-    'STOPPED': AgentCheck.CRITICAL,
-    'STARTING': AgentCheck.OK,
-    'RUNNING': AgentCheck.OK,
-    'BACKOFF': AgentCheck.UNKNOWN,
-    'STOPPING': AgentCheck.WARNING,
-    'EXITED': AgentCheck.CRITICAL,
-    'FATAL': AgentCheck.CRITICAL,
-    'UNKNOWN': AgentCheck.UNKNOWN
+    'STOPPED': CRITICAL,
+    'STARTING': OK,
+    'RUNNING': OK,
+    'BACKOFF': UNKNOWN,
+    'STOPPING': CRITICAL,
+    'EXITED': CRITICAL,
+    'FATAL': CRITICAL,
+    'UNKNOWN': UNKNOWN
 }
+
+TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+
+def time_formatter(s):
+    return time.strftime(TIME_FORMAT, time.localtime(s))
 
 
 class SupervisordCheck(AgentCheck):
 
     def check(self, instance):
+        name = instance.get('name', DEFAULT_SERVER)
         server = self._connect(instance)
+        count = {
+            AgentCheck.OK: 0,
+            AgentCheck.CRITICAL: 0,
+            AgentCheck.UNKNOWN: 0
+        }
+
+        # Report service checks and uptime for each process
         proc_names = instance.get('proc_names', [])
         for proc_name in proc_names:
-            proc_info = server.supervisor.getProcessInfo(proc_name)
-            status = STATUS_MAP[proc_info['statename']]
-            uptime = proc_info['now'] - proc_info['start']
-            tags = ['supervisord', 'proc_name:%s' % proc_info['name']]
-            self.service_check('supervisord.process.check', status, tags=tags)
+            tags = ['supervisord',
+                    'server:%s' % name,
+                    'process:%s' % proc_name]
+            info = server.supervisor.getProcessInfo(proc_name)
+
+            # Report Service Check
+            status = STATUS_MAP[info['statename']]
+            msg = self._build_message(info)
+            count[status] += 1
+            self.service_check('supervisord.process.check',
+                               status, tags=tags, message=msg)
+            # Report Uptime
+            start, stop, now = int(info['start']), int(info['stop']), int(info['now'])
+            uptime = 0 if stop == 0 else now - start
             self.gauge('supervisord.process.uptime', uptime, tags=tags)
+
+        # Report counts by status
+        tags = ['supervisord', 'server:%s' % name]
+        self.gauge('supervisord.process.total', len(proc_names), tags=tags)
+        self.gauge('supervisord.process.up', count[OK], tags=tags)
+        self.gauge('supervisord.process.down', count[CRITICAL], tags=tags)
+        self.gauge('supervisord.process.unknown', count[UNKNOWN], tags=tags)
 
     def _connect(self, instance):
         host = instance.get('host', DEFAULT_HOST)
@@ -36,3 +74,20 @@ class SupervisordCheck(AgentCheck):
         password = instance.get('pass', None)
         auth = '%s:%s@' % (user, password) if user and password else ''
         return xmlrpclib.Server('http://%s%s:%s/RPC2' % (auth, host, port))
+
+    def _build_message(self, proc):
+        proc['now_str'] = time_formatter(proc['now'])
+        proc['start_str'] = time_formatter(proc['start'])
+        proc['stop_str'] = '' if proc['stop'] == 0 else time_formatter(proc['stop'])
+
+        return """Current time: %(now_str)s
+Process name: %(name)s
+Process group: %(group)s
+Description: %(description)s
+Error log file: %(stderr_logfile)s
+Stdout log file: %(stdout_logfile)s
+Log file: %(logfile)s
+State: %(statename)s
+Start time: %(start_str)s
+Stop time: %(stop_str)s
+Exit Status: %(exitstatus)s""" % proc
